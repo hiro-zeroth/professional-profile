4b19ffba6c4b30db5283f60d53c8c75e670afe50
Michelle <your_github_email@example.com>
2025-10-09 00:08:06 +0800

    fixed particles

--- js/particles-config.js
+++ js/particles-config.js
@@ -1,19 +1,15 @@
-/* ---- particles.js config ---- */
 particlesJS("particles-js", {
   particles: {
     number: {
-      value: 380,
+      value: 120,
       density: { enable: true, value_area: 800 },
     },
     color: { value: "#ffffff" },
     shape: {
       type: "circle",
-      stroke: { width: 0, color: "#000000" },
-      polygon: { nb_sides: 5 },
     },
     opacity: {
       value: 0.5,
-      random: false,
     },
     size: {
       value: 3,
@@ -28,52 +24,26 @@ particlesJS("particles-js", {
     },
     move: {
       enable: true,
-      speed: 6,
+      speed: 2,
       direction: "none",
       random: false,
       straight: false,
       out_mode: "out",
       bounce: false,
+      attract: { enable: true, rotateX: 600, rotateY: 1200 }
     },
   },
   interactivity: {
     detect_on: "canvas",
     events: {
-      onhover: { enable: true, mode: "grab" },
+      onhover: { enable: true, mode: "attract" },
       onclick: { enable: true, mode: "push" },
       resize: true,
     },
     modes: {
-      grab: {
-        distance: 140,
-        line_linked: { opacity: 1 },
-      },
+      attract: { distance: 200, duration: 0.4 },
       push: { particles_nb: 4 },
     },
   },
   retina_detect: true,
 });
-
-/* ---- stats.js config ---- */
-var count_particles, stats, update;
-stats = new Stats();
-stats.setMode(0);
-stats.domElement.style.position = "absolute";
-stats.domElement.style.left = "0px";
-stats.domElement.style.top = "0px";
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector(".js-count-particles");
-
-update = function () {
-  stats.begin();
-  stats.end();
-  if (
-    window.pJSDom[0].pJS.particles &&
-    window.pJSDom[0].pJS.particles.array
-  ) {
-    count_particles.innerText =
-      window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
