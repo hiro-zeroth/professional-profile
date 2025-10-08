66db206fd86f8c004d12de806550053ac8eeaf53
Michelle <your_github_email@example.com>
2025-10-08 23:11:53 +0800

    Added particles background and navigation

---
+++ js/particle-config.js
@@ -0,0 +1,30 @@
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80, density: { enable: true, value_area: 800 } },
+    color: { value: "#ffffff" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+    size: { value: 3 },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: { enable: true, speed: 3 },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "repulse" },
+      onclick: { enable: true, mode: "push" },
+      resize: true,
+    },
+    modes: {
+      repulse: { distance: 100 },
+      push: { particles_nb: 4 },
+    },
+  },
+  retina_detect: true,
+});
