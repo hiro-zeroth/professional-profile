6eda2644761eeb3904ddae4dac145533a7cec3b6
Michelle <your_github_email@example.com>
2025-10-08 23:32:05 +0800

    fixed particles mweheh :3

--- js/particles-config.js
+++ js/particles-config.js
@@ -1,28 +1,36 @@
 particlesJS("particles-js", {
   particles: {
-    number: { value: 80, density: { enable: true, value_area: 800 } },
-    color: { value: "#ffffff" },
+    number: { value: 90, density: { enable: true, value_area: 800 } },
+    color: { value: "#66fcf1" },
     shape: { type: "circle" },
-    opacity: { value: 0.5 },
+    opacity: { value: 0.6 },
     size: { value: 3 },
     line_linked: {
       enable: true,
       distance: 150,
-      color: "#ffffff",
+      color: "#66fcf1",
       opacity: 0.4,
       width: 1,
     },
-    move: { enable: true, speed: 3 },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      attract: { enable: false },
+    },
   },
   interactivity: {
     detect_on: "canvas",
     events: {
-      onhover: { enable: true, mode: "repulse" },
+      onhover: { enable: true, mode: "grab" }, // "grab" connects to the cursor
       onclick: { enable: true, mode: "push" },
       resize: true,
     },
     modes: {
-      repulse: { distance: 100 },
+      grab: { distance: 180, line_linked: { opacity: 0.8 } },
       push: { particles_nb: 4 },
     },
   },
